c2550a13229231a4a1e2886bdf562786f9c8b845
Krishna <krishna@assistanz.com>
2016-01-22 20:34:10 +0530

    App service comment correction done.

--- src/main/webapp/scripts/factories/appService.js
+++ src/main/webapp/scripts/factories/appService.js
@@ -11,7 +11,7 @@ function appService(crudService, localStorageService, globalConfig, promiseAjax,
 	// Global configuration to add configuration vlaues
     object.globalConfig = globalConfig;
 
-    // Application ajax calls goes here
+    // Promise ajax call goes here
     object.promiseAjax = promiseAjax
 
     // Application notification
@@ -20,7 +20,7 @@ function appService(crudService, localStorageService, globalConfig, promiseAjax,
     // Applicaiton Utilities
     object.utilService = utilService
 
-    // Application ajax calls goes here
+    // Dialog Service call goes here
     object.dialogService = dialogService
 
     // Model service calls goes here
