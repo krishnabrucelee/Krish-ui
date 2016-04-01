83591e73661f301adbf3d7e0befacf46ee0eeabd
Ibrahim <ibrahim@assistanz.com>
2016-04-02 04:47:29 +0530

    websocket issue fixed for secondary ip

--- src/main/webapp/scripts/factories/webSocket.js
+++ src/main/webapp/scripts/factories/webSocket.js
@@ -58,7 +58,8 @@ function webSocket($rootScope, $timeout, webSockets, globalConfig, notify) {
                     .indexOf("Nic") > -1 || message.body.indexOf("uploading volume") > -1 || message.body
                     .indexOf("VM snapshot") > -1 || message.body.indexOf("vm snapshots") > -1 || message.body
                     .indexOf("deleting snapshot") > -1 || message.body.indexOf("static nat") > -1) || message.body
-                    .indexOf("remote access vpn") > -1 || message.body.indexOf("VPN user") > -1) {
+                    .indexOf("remote access vpn") > -1 || message.body.indexOf("VPN user") > -1 || message.body
+                    .indexOf("secondary ip rules") > -1) {
                 notify({
                     message : message.body,
                     classes : 'alert-success',
@@ -71,7 +72,7 @@ function webSocket($rootScope, $timeout, webSockets, globalConfig, notify) {
                     .indexOf("uploading volume") > -1 || message.body.indexOf("VM snapshot") > -1 || message.body
                     .indexOf("deleting snapshot") > -1 || message.body.indexOf("vm snapshots") > -1 || message.body
                     .indexOf("static nat") > -1) || message.body.indexOf("remote access vpn") > -1 || message.body
-                    .indexOf("VPN user") > -1) {
+                    .indexOf("VPN user") > -1 || message.body.indexOf("secondary ip rules") > -1) {
                 notify({
                     message : message.body,
                     classes : 'alert-danger',
@@ -102,33 +103,12 @@ function webSocket($rootScope, $timeout, webSockets, globalConfig, notify) {
                 }
             }
         });
-        webSockets.subscribe("/topic/action.event/" + msg + "/" + id, function(message) {
-            if (message.body.indexOf("Successfully") > -1) {
-                notify({
-                    message : message.body,
-                    classes : 'alert-success',
-                    templateUrl : globalConfig.NOTIFICATION_TEMPLATE
-                });
-                $rootScope.$broadcast(msg, 'action.event', 'success', id, userId);
-            } else if (message.body.indexOf("Error") > -1) {
-                notify({
-                    message : message.body,
-                    classes : 'alert-danger',
-                    templateUrl : globalConfig.NOTIFICATION_TEMPLATE
-                });
-                if (message.body.indexOf("SSHKey") > -1) {
-
-                } else {
-                    $rootScope.$broadcast(msg, 'action.event', 'error', id, userId);
-                }
-            }
-        });
         webSockets
                 .subscribe("/topic/async.event/" + msg + "/" + userId + "/" + id, function(message) {
                     if (msg.indexOf("FIREWALL.EGRESS") > -1 || msg.indexOf("NET.IP") > -1 || msg
                             .indexOf("FIREWALL.OPEN") > -1 || msg.indexOf("FIREWALL.CLOSE") > -1 || msg
                             .indexOf("NET.RULEADD") > -1 || msg.indexOf("NET.RULEDELETE") > -1 || msg
-                            .indexOf("VM.RESETPASSWORD") > -1) {
+                            .indexOf("VM.RESETPASSWORD") > -1 || msg.indexOf("NIC.SECONDARY") > -1) {
 
                     } else {
                         $rootScope.$broadcast(msg, 'async.event', 'success', id, userId);
