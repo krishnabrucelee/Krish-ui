496b71e47238a6411aa0681ab67c937f1934c4a9
Krishna <krishna@assistanz.com>
2016-04-20 18:53:08 +0530

    Remember login redirect to dashboard done.

--- src/main/webapp/scripts/controllers/loginController.js
+++ src/main/webapp/scripts/controllers/loginController.js
@@ -5,6 +5,19 @@
  */
 angular.module('homer', ['ngCookies']).controller("loginCtrl", function ($scope, $http, globalConfig, $window, $remember, $cookies) {
 
+	//For remember login functionality.
+    if (($cookies.rememberMe == "true")) {
+		console.log("hiqwe");
+		return $http({method:'get', url: 'http://'+ $window.location.hostname +':8080/api/'  + 'users/usersessiondetails/'+$cookies.id,
+			"headers": {'x-auth-token': $cookies.token, 'x-requested-with': '', 'Content-Type': 'application/json', 'Range': "items=0-9", 'x-auth-login-token': $cookies.loginToken, 'x-auth-remember': $cookies.rememberMe, 'x-auth-user-id': $cookies.id, 'x-auth-login-time': $cookies.loginTime}})
+			.then(function(result){
+				$window.location.href = "index#/dashboard";
+          }, function(errorResponse) {
+        	  console.log(errorResponse);
+        	  $cookies.rememberMe = "false";
+        	  $window.location.reload();
+        });
+	}
         $scope.loginForm = function () {
     	if (angular.isUndefined($scope.user_remember)) {
     		$scope.user_remember = "false";
@@ -84,4 +97,13 @@ angular.module('homer', ['ngCookies']).controller("loginCtrl", function ($scope,
 
     }
 
+//        if (($cookies.rememberMe != "false" || !angular.isUndefined($cookies.rememberMe)) && ($cookies.id != null || !angular.isUndefined($cookies.id))) {
+//        	return $http({method:'get', url: 'http://'+ $window.location.hostname +':8080/api/'  + 'users/usersessiondetails/'+$cookies.id,
+//    			"headers": {'x-auth-token': $cookies.token, 'x-requested-with': '', 'Content-Type': 'application/json', 'Range': "items=0-9", 'x-auth-login-token': $cookies.loginToken, 'x-auth-remember': $cookies.rememberMe, 'x-auth-user-id': $cookies.id, 'x-auth-login-time': $cookies.loginTime}})
+//    			.then(function(result){
+//    				$window.location.href = "index#/dashboard";
+//              }, function(errorResponse) {
+//            	  console.log(errorResponse);
+//            });
+//    	}
 });
