e0b9b9f7a8c5859c7d8a6873da26fe4463a0e3d4
Mohanapriya <Mohanapriya@assistanz.com>
2016-05-10 09:34:21 +0530

    special character search in quick search added for instance,application,role

--- src/main/webapp/scripts/controllers/projectController.js
+++ src/main/webapp/scripts/controllers/projectController.js
@@ -566,12 +566,14 @@ if ($scope.domainView != null && $scope.vmSearch == null) {
                         appService.notify({message: 'Project deleted successfully ', classes: 'alert-success', templateUrl: $scope.global.NOTIFICATION_TEMPLATE});
 
                     }).catch(function (result) {
-				if(result.statusText == "Precondition Failed") {
+				
                   	 if(!angular.isUndefined(result) && result.data != null) {
       		 		   	if(result.data.globalError[0] != '' && !angular.isUndefined(result.data.globalError[0])){
 	                      	 var errorMsgs = result.data.globalError[0];
 	                      	 var errorList = errorMsgs.split("@@");
 	                     	 appService.notify.closeAll();
+				console.log(errorList[1]);
+				if(errorList[1] != undefined) {
 	                      		var msg = "You have following resources for this project: <br><ul>" +
 	                  	 		"<li>Instance :"+ errorList[1] +"</li>" +
 	                  	 		"<li>Network :"+ errorList[2] + "</li>" +
@@ -580,13 +582,14 @@ if ($scope.domainView != null && $scope.vmSearch == null) {
 	                  	 		"</ul><br>Kindly delete associated resources and try again";
 	                      		appService.notify({message: msg, classes: 'alert-danger', templateUrl: $scope.global.NOTIFICATION_TEMPLATE });
 	                       }
+			  }
 
 	                       angular.forEach(result.data.fieldErrors, function(errorMessage, key) {
 	                      	$scope.departmentForm[key].$invalid = true;
 	                       	$scope.departmentForm[key].errorMessage = errorMessage;
 	                       });
            			  }
-			}
+			
 
                    });
                     $modalInstance.close();
