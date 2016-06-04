5f9ab91be1b02ea9911089263e30323afd295386
Ibrahim <ibrahim@assistanz.com>
2016-06-04 23:10:07 +0530

    monitoing code updated

--- src/main/webapp/scripts/factories/webSocket.js
+++ src/main/webapp/scripts/factories/webSocket.js
@@ -7,7 +7,7 @@ function webSocket($rootScope, $timeout, webSockets, globalConfig, notify) {
     var headers = {};
 
     var initStompClient = function() {
-        webSockets.init(globalConfig.SOCKET_URL + 'socket/ws');
+        webSockets.init(globalConfig.SOCKET_URL + 'socket/ws', null);
         headers['x-auth-token'] = globalConfig.sessionValues.token;
         $rootScope.initiateConnection++;
         webSockets
