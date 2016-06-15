b5b0aa00718bba951a364993469b2b139b799f86
Mohanapriya <Mohanapriya@assistanz.com>
2016-06-15 12:07:21 +0530

    alignment issue fixed

--- src/main/java/ck/panda/jadeart/CaptchaServlet.java
+++ src/main/java/ck/panda/jadeart/CaptchaServlet.java
@@ -1,6 +1,5 @@
 package ck.panda.jadeart;
 
-import java.awt.AlphaComposite;
 
 /*
  * To change this template, choose Tools | Templates
@@ -11,12 +10,9 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.GradientPaint;
 import java.awt.Graphics2D;
-import java.awt.GraphicsEnvironment;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.*;
-import java.net.*;
-
 import java.util.Random;
 import javax.imageio.ImageIO;
 import javax.servlet.*;
@@ -25,7 +21,9 @@ public class CaptchaServlet extends HttpServlet {
     
 protected TextOptions options;
 	
+	/** Text options. */
 	public enum TextOptions{
+		/** Number format only.*/
 		NUMBERS_ONLY
 	}
 
@@ -44,42 +42,31 @@ protected TextOptions options;
         { '5', '9', '8'}
     };
 
-
     BufferedImage bufferedImage = new BufferedImage(width, height, 
                   BufferedImage.TYPE_INT_RGB);
-
     Graphics2D g2d = bufferedImage.createGraphics();
-
     Font font = new Font("Utopia", Font.BOLD, 18);
     g2d.setFont(font);
-
     RenderingHints rh = new RenderingHints(
            RenderingHints.KEY_ANTIALIASING,
            RenderingHints.VALUE_ANTIALIAS_ON);
-
     rh.put(RenderingHints.KEY_RENDERING, 
            RenderingHints.VALUE_RENDER_QUALITY);
-
     g2d.setRenderingHints(rh);
 
     Color test = new Color(143, 203, 233);
     Color grey1 = new Color(0,0,0);
-
-    
-    GradientPaint gp = new GradientPaint(0, 0, 
-    		grey1, 0, height/2,test, false);
-
+    GradientPaint gp = new GradientPaint(0, 0, grey1, 0, height/2,test, false);
     g2d.setPaint(gp);
     g2d.fillRect(0, 0, width, height);
-
     g2d.setColor(new Color(0, 0, 0));
-     Random r = new Random(System.currentTimeMillis());
-	    CharArrayWriter cab = new CharArrayWriter();
-	   String answer = "";
-		for(int i = 0; i < 5; i ++){
-			int u_l_n = r.nextInt(1);
-			char ch = ' ';
-		   switch(u_l_n){
+    Random r = new Random(System.currentTimeMillis());
+	CharArrayWriter cab = new CharArrayWriter();
+	String answer = "";
+	for(int i = 0; i < 5; i ++){
+		int u_l_n = r.nextInt(1);
+		char ch = ' ';
+		switch(u_l_n){
 		    //Numbers
 		    case 0:
 		    	ch = (char)(r.nextInt(58 - 49) + (49));
@@ -89,15 +76,13 @@ protected TextOptions options;
 			answer += ch;
 		}
 		
-	    char[] data1 = cab.toCharArray();
+	char[] data1 = cab.toCharArray();
     Random r1 = new Random();
     int index = Math.abs(r1.nextInt()) % 5;
-
     String captcha = String.copyValueOf(data1);
     request.getSession().setAttribute("captcha", captcha );
     int x = 0; 
     int y = 0;
-
     for (int i=0; i<data1.length; i++) {
         x += 10 + (Math.abs(r.nextInt()) % 15);
         y = 20 + Math.abs(r.nextInt()) % 20;
@@ -108,11 +93,12 @@ protected TextOptions options;
     OutputStream os = response.getOutputStream();
     ImageIO.write(bufferedImage, "png", os);
     os.close();
-				
-    
   } 
 
-
+  /**
+   * HttpServlet get method with request and response .
+   * 
+   */
   protected void doGet(HttpServletRequest request, 
                        HttpServletResponse response)
                            throws ServletException, IOException {
@@ -120,20 +106,23 @@ protected TextOptions options;
       
   } 
 
-
+  /**
+   * HttpServlet post method with request and response .
+   * 
+   */
   protected void doPost(HttpServletRequest request, 
                         HttpServletResponse response)
                             throws ServletException, IOException {
-	  String answer = "";
-      String captcha = null;
-      String captchas = (String) request.getSession().getAttribute("captcha");
-  		request.getHeader("x-answer");
-  		response.setContentType("application/json");
-  		answer = request.getHeader("x-answer");
-  		if	(captchas.equals(answer)) {
+	 String answer = "";
+     String captcha = null;
+     String captchas = (String) request.getSession().getAttribute("captcha");
+  	 request.getHeader("x-answer");
+  	 response.setContentType("application/json");
+  	 answer = request.getHeader("x-answer");
+  	 if	(captchas.equals(answer)) {
   			response.getWriter().write("{\"result\":\"" +"success"+ "\"}");
-  		} else{
+  	} else{
   			response.getWriter().write("{\"result\":\"" +"failure"+ "\"}");
-  		}
   	}
+  }
 }
