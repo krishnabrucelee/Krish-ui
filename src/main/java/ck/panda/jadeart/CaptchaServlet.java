ed647ddc6016b6ed4016cbd99bad14c752f17989
Mohanapriya <Mohanapriya@assistanz.com>
2016-06-20 11:50:14 +0530

    alignment changes added in captcha

--- src/main/java/ck/panda/jadeart/CaptchaServlet.java
+++ src/main/java/ck/panda/jadeart/CaptchaServlet.java
@@ -18,21 +18,21 @@ import javax.imageio.ImageIO;
 import javax.servlet.*;
 import javax.servlet.http.*;
 public class CaptchaServlet extends HttpServlet {
-    
+
 protected TextOptions options;
-	
+
 	/** Text options. */
 	public enum TextOptions{
 		/** Number format only.*/
 		NUMBERS_ONLY
 	}
 
-  protected void processRequest(HttpServletRequest request, 
-                                HttpServletResponse response) 
+  protected void processRequest(HttpServletRequest request,
+                                HttpServletResponse response)
                  throws ServletException, IOException {
 
     int width = 125;
-    int height = 40;
+    int height = 38;
 
     char data[][] = {
         { '1', '5', '7', '6', '3', '2' },
@@ -42,7 +42,7 @@ protected TextOptions options;
         { '5', '9', '8'}
     };
 
-    BufferedImage bufferedImage = new BufferedImage(width, height, 
+    BufferedImage bufferedImage = new BufferedImage(width, height,
                   BufferedImage.TYPE_INT_RGB);
     Graphics2D g2d = bufferedImage.createGraphics();
     Font font = new Font("Utopia", Font.BOLD, 18);
@@ -50,13 +50,13 @@ protected TextOptions options;
     RenderingHints rh = new RenderingHints(
            RenderingHints.KEY_ANTIALIASING,
            RenderingHints.VALUE_ANTIALIAS_ON);
-    rh.put(RenderingHints.KEY_RENDERING, 
+    rh.put(RenderingHints.KEY_RENDERING,
            RenderingHints.VALUE_RENDER_QUALITY);
     g2d.setRenderingHints(rh);
 
     Color test = new Color(143, 203, 233);
     Color grey1 = new Color(0,0,0);
-    GradientPaint gp = new GradientPaint(0, 0, grey1, 0, height/2,test, false);
+    GradientPaint gp = new GradientPaint(0, 0, test, 0, height/2,test, false);
     g2d.setPaint(gp);
     g2d.fillRect(0, 0, width, height);
     g2d.setColor(new Color(0, 0, 0));
@@ -70,47 +70,47 @@ protected TextOptions options;
 		    //Numbers
 		    case 0:
 		    	ch = (char)(r.nextInt(58 - 49) + (49));
-		    	break;			    	
+		    	break;
 		    }
 			cab.append(ch);
 			answer += ch;
 		}
-		
+
 	char[] data1 = cab.toCharArray();
     Random r1 = new Random();
     int index = Math.abs(r1.nextInt()) % 5;
     String captcha = String.copyValueOf(data1);
     request.getSession().setAttribute("captcha", captcha );
-    int x = 0; 
+    int x = 0;
     int y = 0;
     for (int i=0; i<data1.length; i++) {
         x += 10 + (Math.abs(r.nextInt()) % 15);
         y = 20 + Math.abs(r.nextInt()) % 20;
         g2d.drawChars(data1, i, 1, x, y);
     }
-    g2d.dispose(); 
+    g2d.dispose();
     response.setContentType("image/png");
     OutputStream os = response.getOutputStream();
     ImageIO.write(bufferedImage, "png", os);
     os.close();
-  } 
+  }
 
   /**
    * HttpServlet get method with request and response .
-   * 
+   *
    */
-  protected void doGet(HttpServletRequest request, 
+  protected void doGet(HttpServletRequest request,
                        HttpServletResponse response)
                            throws ServletException, IOException {
       processRequest(request, response);
-      
-  } 
+
+  }
 
   /**
    * HttpServlet post method with request and response .
-   * 
+   *
    */
-  protected void doPost(HttpServletRequest request, 
+  protected void doPost(HttpServletRequest request,
                         HttpServletResponse response)
                             throws ServletException, IOException {
 	 String answer = "";
